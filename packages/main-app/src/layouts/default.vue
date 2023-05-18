dd8b239a498afef192392c913f647e3c5e369c1e
taokepppooo <1075626521@qq.com>
2023-05-18 10:21:35 +0800

    feat(main-app): 添加快捷图标导航栏

--- packages/main-app/src/layouts/default.vue
+++ packages/main-app/src/layouts/default.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import { useNamespace } from '@/hooks/useNamespace'
-import '~comp/layouts.less'
 
 const ns = useNamespace('layouts')
 </script>
 
 <template>
   <div :class="ns.b()">
-    <LayoutsVerticalNav></LayoutsVerticalNav>
-    <LayoutsViewer></LayoutsViewer>
+    <LayoutsViewer />
+    <LayoutsQuickNav />
   </div>
 </template>
+
+<style lang="less">
+@ns: ~'@{namespace}-layouts';
+
+.@{ns} {
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  background: url('https://cdn.dribbble.com/userupload/6903601/file/original-34da2d73df7ef84fcf35be4f456b0d87.png?compress=1&resize=1600x1280');
+  background-size: cover;
+}
+</style>
