10ae280a0ad421c84128ba6a9a9ca902ba8c47a5
taokepppooo <1075626521@qq.com>
2023-05-24 17:03:14 +0800

    feat(main-app): 导航图标添加拖拽功能

---
+++ packages/main-app/src/hooks/useSortable.ts
@@ -0,0 +1,17 @@
+import type { SortableOptions } from 'sortablejs'
+
+export const useSortable = (el: HTMLElement, options?: SortableOptions) => {
+  const init = async () => {
+    if (!el) return
+
+    const Sortable = (await import('sortablejs')).default
+    Sortable.create(unref(el), {
+      animation: 300,
+      delay: 300,
+      delayOnTouchOnly: true,
+      ...options
+    })
+  }
+
+  init()
+}
