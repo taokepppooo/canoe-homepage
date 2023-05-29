bba44f53417857123e9c39f6ca5057ee810d03ac
taokepppooo <1075626521@qq.com>
2023-05-29 17:53:26 +0800

    feat(main-app): stage

--- packages/main-app/src/views/desktop/index.vue
+++ packages/main-app/src/views/desktop/index.vue
@@ -3,6 +3,7 @@ import { useNamespace } from '@/hooks/useNamespace'
 import { useSortable } from '@/hooks/useSortable'
 import { useDesktopGlobal } from '@/hooks/useGlobal'
 import { useDesktop } from '@/hooks/desktop/useDesktop'
+import type { MoveEvent } from 'sortablejs'
 
 const ns = useNamespace('desktop')
 const { appCSSConstant, appSize } = useDesktopGlobal()
@@ -20,7 +21,44 @@ for (let i = 0; i < 100; i++) {
 
 nextTick(() => {
   const element = appsRef.value
-  useSortable(element)
+
+  useSortable(element, {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    onMove: (evt: any) => {
+      console.log(evt)
+      console.log(evt.originalEvent)
+      const related = evt.related
+      const dragged = evt.dragged
+      const offsetX = related.offsetLeft - dragged.offsetLeft
+      const offsetY = related.offsetTop - dragged.offsetTop
+
+      let targetInnerXPosition = 0
+      let targetInnerYPosition = 0
+      // 横向和纵向都变动
+      if (offsetX && offsetY) {
+        targetInnerXPosition =
+          offsetX % (parseInt(appCSSConstant.value.gridGapX) + parseInt(appSize.value.width))
+        targetInnerYPosition =
+          offsetY % (parseInt(appCSSConstant.value.gridGapY) + parseInt(appSize.value.height))
+      } else {
+        // 只有横向变动
+        if (offsetX) {
+          targetInnerXPosition =
+            offsetX % (parseInt(appCSSConstant.value.gridGapX) + parseInt(appSize.value.width))
+          console.log(targetInnerXPosition, 'x')
+        }
+
+        // 只有纵向变动
+        if (offsetY) {
+          targetInnerYPosition =
+            offsetY % (parseInt(appCSSConstant.value.gridGapY) + parseInt(appSize.value.height))
+          console.log(offsetY, '1')
+          console.log(parseInt(appCSSConstant.value.gridGapY) + parseInt(appSize.value.height), '2')
+          console.log(targetInnerYPosition, 'y')
+        }
+      }
+    }
+  })
 
   useDesktop(
     desktopHeight,
